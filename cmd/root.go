f016b6c5a09ff5e6c3b3477d4de302b0b2a15126
ei-sugimoto <eikun1014@gmail.com>
2024-06-27 23:30:56 +0900

    feat: add `main` and add `root` and install `cobra`

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "mycli",
+	Short: "A CLI tool for generating project templates",
+	Long:  `A CLI tool that helps you generate project templates quickly.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome to mycli!")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ei-sugimoto/9ick/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
